e30b98cbc3a5b283af8e40717ad153083894f326
lastone-hikiray <vlasios2016@gmail.com>
2021-07-19 22:42:55 +0300

    Initial commit

---
+++ Assets/Script/MoveToMouse.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+
+public class MoveToMouse : MonoBehaviour
+{
+
+    [SerializeField] private float Speed;
+    [SerializeField] private float DistanceFromCamera;
+    [SerializeField] private Camera usedCamera;
+    private void Reset()
+    {
+        usedCamera = Camera.main;
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log(Mathf.Exp(-i));
+        }
+    }
+
+    void Update()
+    {
+        Vector3 MousePosition = Input.mousePosition;
+        MousePosition.z = DistanceFromCamera;
+
+        Vector3 MouseScreenToWorld = usedCamera.ScreenToWorldPoint(MousePosition);
+        Vector3 Position = Vector3.Lerp(transform.position, MouseScreenToWorld, 1.0f - Mathf.Exp(-Speed * Time.deltaTime));
+
+        transform.position = Position;
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# ParticleSystemSamples
+
+The project uses Unity-2021.1.12.f1
